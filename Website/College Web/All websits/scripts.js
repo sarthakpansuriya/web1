7ff8cee6b3c8f28913ea6098316706db2181c9ee
sarthakpansuriya <sarthakpansuriya912@gmail.com>
2025-01-09 18:01:54 +0530

    Add files via upload
    good

---
+++ Website/College Web/All websits/scripts.js
@@ -0,0 +1,7 @@
+// Custom JavaScript
+
+// Example: You can add interactive JS here, like handling form submissions, animations, etc.
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Welcome to Nutrition Hub!");
+});
